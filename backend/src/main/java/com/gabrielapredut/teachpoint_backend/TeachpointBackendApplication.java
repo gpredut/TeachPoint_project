d4bf6f7ddc3c3e16c460e3ca657e3479cbf1c863
Gabriela Predut <gabrielaa.predut@gmail.com>
2024-07-23 17:55:27 +0300

    Add backend and frontend projects

---
+++ backend/src/main/java/com/gabrielapredut/teachpoint_backend/TeachpointBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielapredut.teachpoint_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachpointBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachpointBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gabrielapredut/teachpoint_backend/TeachpointBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielapredut.teachpoint_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachpointBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
